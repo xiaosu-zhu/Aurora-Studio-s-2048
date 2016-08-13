6671c8f3d6c9265a52751915a6ae8063bc0bbf8e
pkzxs <pkzxs123@gmail.com>
2016-08-13 23:24:00 +0800

    first commit

---
+++ Aurora.Studio.2048.Core/Game/Calculator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Aurora.Shared.Extensions;
+using Com.Aurora.Shared.Helpers;
+
+namespace Aurora.Studio._2048.Core.Game
+{
+    public static class Calculator
+    {
+        public static int[][] New()
+        {
+            var p = new int[][] { new int[] { 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0 } };
+            Add(ref p);
+            Add(ref p);
+            return p;
+        }
+
+        public static void Add(ref int[][] matrix)
+        {
+            int i = 0, j = 0;
+            List<int> row = new List<int>();
+            List<int> col = new List<int>();
+            if (matrix != null && matrix.Length == 4)
+            {
+                foreach (var m_row in matrix)
+                {
+                    if (m_row != null && m_row.Length == 4)
+                    {
+                        j = 0;
+                        foreach (var item in m_row)
+                        {
+                            if (item == 0)
+                            {
+                                row.Add(i);
+                                col.Add(j);
+                            }
+                            j++;
+                        }
+                        i++;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Not a valid matrix");
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Not a valid matrix");
+            }
+            var num = Tools.Random.Next(row.Count);
+            matrix[row[num]][col[num]] = Tools.RandomBool() ? 2 : 4;
+        }
+
+        public static bool MoveLeft(ref int[][] matrix)
+        {
+            int i, j;
+            var b = false;
+            foreach (var row in matrix)
+            {
+                for (i = 0, j = 1; i < row.Length; i++, j = i + 1)
+                {
+                    if (j == row.Length)
+                        break;
+                    while (j < row.Length && row[j++] == 0) ;
+                    if (row[i] == 0)
+                    {
+                        row[i] = row[j - 1];
+                        row[j - 1] = 0;
+                    }
+                    if (row[i] == row[--j])
+                    {
+                        row[i] *= 2;
+                        row[j] = 0;
+                        b = true;
+                        continue;
+                    }
+                    if (i + 1 != j)
+                    {
+                        row[i + 1] = row[j];
+                        b = true;
+                    }
+                }
+            }
+            return b;
+        }
+    }
+}
---
+++ Tests/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Aurora.Studio._2048.Core.Game;
+using Com.Aurora.Shared.Extensions;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MoveLeftTest()
+        {
+            var p = Calculator.New();
+            Calculator.MoveLeft(ref p);
+            p = new int[][] { new int[] { 2, 2, 2, 2 }, new int[] { 2, 4, 0, 0 }, new int[] { 2, 0, 2, 0 }, new int[] { 4, 4, 0, 2 } };
+            var k = new int[][] { new int[] { 4, 4, 0, 0 }, new int[] { 2, 4, 0, 0 }, new int[] { 4, 0, 0, 0 }, new int[] { 8, 2, 0, 0 } };
+            Calculator.MoveLeft(ref p);
+            Assert.AreEqual(k, p);
+        }
+    }
+}
